c4c72a78b14b5eb16ea0f70ba7006e20782bc2ee
Damiel Rice <damielrice@gmail.com>
2026-03-29 09:04:12 +0700

    use data from the backend

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,14 +1,18 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
-import { products } from '../../starting-code/data/products';
 import './HomePage.css';
 import homeicon from '../assets/images/home-icon.png';
 
 export function HomePage() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
   axios.get('http://localhost:3000/api/products')
-  .then((response) => {
-  console.log(response.data);
+    .then((response) => {
+    setProducts(response.data);
     });
+  }, []);
+ 
 return (
 
 
