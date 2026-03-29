48912f7af68569e05d9640d09c612a632ebbe43e
Damiel Rice <damielrice@gmail.com>
2026-03-29 14:18:01 +0700

    lift state up and share it between pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { HomePage } from './Pages/HomePage';
 import { CheckoutPage } from './Pages/checkout/CheckoutPage';
 import { OrdersPage } from './Pages/OrdersPage';
@@ -7,11 +9,19 @@ import { NotFound } from './Pages/NotFound';
 import './App.css'
 
 function App() {
+   const [cart, setCart] = useState([]);
+  
+   useEffect(() => {
+   axios.get('/api/cart-items')
+    .then((response) => {
+     setCart(response.data);
+    });
+    }, []);
 
   return (
     <Routes>
-      <Route index element={ <HomePage />}/>
-      <Route path="checkout" element={<CheckoutPage />}></Route>
+      <Route index element={ <HomePage cart={cart} />}/>
+      <Route path="checkout" element={<CheckoutPage cart={cart} />}></Route>
       <Route path="orders" element={<OrdersPage />} />
       <Route path="tracking" element={<TrackingPage />} />
       <Route path="*" element={<NotFound />} />
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,19 +4,16 @@ import { Header } from '../components/Header';
 import './HomePage.css';
 import homeicon from '../assets/images/home-icon.png';
 
-export function HomePage() {
+export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
+ 
   useEffect(() => {
   axios.get('/api/products')
     .then((response) => {
     setProducts(response.data);
     });
 
-    axios.get('/api/cart-items')
-    .then((response) => {
-     setCart(response.data);
-    })
+    
   }, []);
  
 return (
