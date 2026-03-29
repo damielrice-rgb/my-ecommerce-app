e85a89794b733ebf02d861fbc3c5389968bea10f
Damiel Rice <damielrice@gmail.com>
2026-03-29 08:57:19 +0700

    use axios to get data

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,8 +1,14 @@
+import axios from 'axios';
 import { Header } from '../components/Header';
 import { products } from '../../starting-code/data/products';
 import './HomePage.css';
 import homeicon from '../assets/images/home-icon.png';
+
 export function HomePage() {
+  axios.get('http://localhost:3000/api/products')
+  .then((response) => {
+  console.log(response.data);
+    });
 return (
 
 
@@ -69,7 +75,7 @@ return (
          );
         })}
 
-      
+  
       </div>
     </div>
    </>
